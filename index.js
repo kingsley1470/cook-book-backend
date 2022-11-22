ce071134443f0031cdc797f79bb7715f379db472
Nizami Suleymanov <nizami.suleymanoff@gmail.com>
2022-11-23 00:39:57 +0100

    removed comments

--- index.js
+++ index.js
@@ -4,18 +4,7 @@ const fileSystem = require('fs');
 const port = 3030;
 const axios = require('axios');
 const path = require('path');
-// app.get('/receipslist', (req, res) =>{
-//     axios.get('http://jsonplaceholder.typicode.com/posts/1')
-//     .then(response =>{
-//         let package = JSON.stringify(response.data)
-//         console.log(package)
-//         fileSystem.writeFile('receipes.json', package, function (err) {
-//             console.log(err);
-//             res.send("Success")
-//         })
-//     })
-//     .catch(err=>console.log(err))
-// })
+
 const filePath = path.join(process.cwd(), "receipes.json")
 
 app.get('/receipes', (req, res) => {
@@ -48,11 +37,3 @@ fileSystem.readFile(filePath, (err, data) => {
 app.listen(port,()=>{
   console.log(`Server running http://localhost:${port}/`);
 });
-
-// https://axios-http.com/docs/intro
-
-// https://www.geeksforgeeks.org/node-js-fs-writefile-method/
-
-// https://medium.com/geekculture/nodejs-express-axios-and-the-basic-set-up-fe88a0b75dca
-
-// https://stackoverflow.com/questions/61045897/write-a-file-based-on-axios-response
